c01d2ceaf75c0a8f7fedd04e3a629a0fba0ed32b
Noah Lidell <noahlidell@gmail.com>
2016-11-12 00:28:08 +0000

    nov 11

--- p13.py
+++
@@ -1,4 +0,0 @@
-#nums = [int(line.rstrip('\n')) for line in open('numbers-p13.txt') ]
-#print str(sum(nums))[0:10]
-print str(sum([int(line.rstrip('\n')) for line in open('numbers-p13.txt')]))[0:10]
-
--- p16.py
+++
@@ -1,1 +0,0 @@
-print sum([int(d) for d in list(str(2**1000))])
